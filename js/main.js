ea8ddfaf53f0519c7e573f5d398cad839bb77045
Sarvar-Dev <sarvarbekr1203@gmail.com>
2024-01-12 11:00:12 +0500

    Add copy button click handler and settimeout

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
+const elUrlShortener = document.querySelector(".url-shortener")
 const elUrlShortenerForm = document.querySelector(".js-url-shortener-form");
 const elUrlShortenerResults = document.querySelector(".url-shortener__results");
 
-elUrlShortenerForm.addEventListener('submit', function (evt){
-    evt.preventDefault();
 
-    elUrlShortenerResults.classList.add("url-shortener__results--open");
-});
+if(elUrlShortenerForm){
+    elUrlShortenerForm.addEventListener('submit', function (evt){
+        evt.preventDefault();
+        elUrlShortenerResults.classList.add("url-shortener__results--open");
+    });    
+}
+
+
+if(elUrlShortener){
+    elUrlShortener.addEventListener('click',function(evt){
+        if(evt.target.matches('.js-copy-short-link-button')){
+            // CHANGE BUTTON TEXT
+            evt.target.textContent = "Copied!";
+            // change button bgcolor
+            evt.target.classList.add("url-shortener__copy-button--copied");
+            // Reset button text and class after 1 cekund
+            setTimeout(function(){
+                evt.target.textContent = "Copy";
+                evt.target.classList.remove("url-shortener__copy-button--copied");
+            },1000);
+        }
+    });
+}
